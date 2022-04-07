811fcf11c9b185a5acc3245dff542d74a0820516
efecetinkaya <96293596+efecetinkaya@users.noreply.github.com>
2022-04-07 16:13:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace consoleprogramlama
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+          try{
+              Console.WriteLine("Bir Sayı Giriiz");
+              int sayi = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("Girdiğiniz Sayı: " + sayi);
+          }
+          catch(ArgumentNullException ex){
+              Console.WriteLine("Hata :" + ex.Message.ToString());
+          }
+          catch(OverflowException ex){
+              Console.WriteLine("Hata :" + ex.Message.ToString());
+          }
+          catch(FormatException ex){
+              Console.WriteLine("Hata :" + ex.Message.ToString());
+          }
+          finally{
+              Console.WriteLine("İşlem Tamamlandı.");
+          }
+        }
+    }
+}
